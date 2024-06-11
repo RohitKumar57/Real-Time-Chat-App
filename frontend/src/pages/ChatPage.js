ae17ab7a5557f1e7d9910e74ddf5c192c17d790e
Rohit Kumar <rohitkumar190204@gmail.com>
2024-06-11 08:51:24 +0530

    Initial Setupdone

---
+++ frontend/src/pages/ChatPage.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+const ChatPage = () => {
+
+    const [chats, setChats] = useState([])
+
+    const fetchChats = async() =>{
+        const {data} = await axios.get('/api/chat')
+        setChats(data);
+        console.log(data)
+    }
+    useEffect(() => {
+        fetchChats();      
+    }, [])
+    
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+    </div>
+  )
+}
+
+export default ChatPage
