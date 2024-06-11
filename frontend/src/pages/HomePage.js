ef406bdcf5c71e4d9d23dde2ee214d4c21eb5ea1
Rohit Kumar <rohitkumar190204@gmail.com>
2024-06-11 09:38:49 +0530

    Completed UI of Login and SignUp in Homepage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,59 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 
 const HomePage = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
 
 export default HomePage
